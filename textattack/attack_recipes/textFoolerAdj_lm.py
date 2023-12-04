1f951d4e7cbae9ed04c2f02e2c7237a60a5cf318
shivsut <shiva.shankara@utexas.edu>
2023-12-04 09:45:32 +0530

    changes

--- textattack/attack_recipes/textFoolerAdj_lm.py
+++ textattack/attack_recipes/textFoolerAdj_lm.py
@@ -131,7 +131,7 @@ class TextFoolerJin2019Adjusted_LM(AttackRecipe):
             lm_liklihood_constraint(model_path=path)
         )
         constraints.append(
-            LanguageTool(5)
+            LanguageTool(0)
         )
         goal_function = UntargetedClassification(model_wrapper)
         #
